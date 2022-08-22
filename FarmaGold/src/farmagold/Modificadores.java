bbbb283563d61ca85ff143175f7f01aeada44802
liangbinjie <67171031+liangbinjie@users.noreply.github.com>
2022-08-21 23:34:05 -0600

    Update Modificadores.java
    Se agrego funcion para modificar estatus

--- FarmaGold/src/farmagold/Modificadores.java
+++ FarmaGold/src/farmagold/Modificadores.java
@@ -3,9 +3,14 @@ import javax.swing.*;
 
 public class Modificadores {
     
+    public enum Estados {
+        Activo,
+        Inactivo
+    }
+    
     public void modificarEmpleado(Empleados listaEmpleados[]) {
         while(true) {
-            int opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "Que desea modificar?\n(1) Identificacion\n(2) Nombre\n(3) Nombre usuario\n(4) Contrasena"));
+            int opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "Que desea modificar?\n(1) Identificacion\n(2) Nombre\n(3) Nombre usuario\n(4) Contrasena\n(5) Estatus"));
             if (opcion == 1) {
                 // identificacion
                 long identificacion = Long.parseLong(JOptionPane.showInputDialog(null, "Digite la identificacion del empleado a modificar"));
@@ -88,11 +93,39 @@ public class Modificadores {
                 } else {
                     JOptionPane.showMessageDialog(null, "Empleado no encontrado");
                     break;
+                } 
+            } else if (opcion == 5) {
+                // estatus
+                long identificacion = Long.parseLong(JOptionPane.showInputDialog(null, "Digite la identificacion del empleado a modificar"));
+                boolean encontrado = false;
+                int index = 0;
+                for (int i=0; i<listaEmpleados.length; i++) {
+                    if (listaEmpleados[i].getIdentificacion() == identificacion) {
+                        encontrado = true;
+                        index = i;
+                    }
                 }
+                if (encontrado == true) {
+                    // modificamos
+                    if (listaEmpleados[index].getEstado() == Empleados.Estados.Activo) {
+                        listaEmpleados[index].setEstado(Empleados.Estados.Inactivo);
+                    } else {
+                        listaEmpleados[index].setEstado(Empleados.Estados.Activo);
+                    }
+                    break;
+                    
+                } else {
+                    JOptionPane.showMessageDialog(null, "Empleado no encontrado");
+                    break;
+                } 
             }
         }
 
         
     
+    }
+    
+    public void modificarClientes(Clientes listaClientes[]) {
+        
     }
 }
