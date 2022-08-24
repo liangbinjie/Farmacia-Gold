9cad2f17fc521b068c41c4a7bb0db7e1858dc656
Jimena Azofeifa <107661297+JimenaAzofeifaPorras@users.noreply.github.com>
2022-08-24 17:33:41 -0600

    Medicamentos Funciones

--- FarmaGold/src/farmagold/MedicamentosFunciones.java
+++ FarmaGold/src/farmagold/MedicamentosFunciones.java
@@ -1,70 +1,71 @@
 package farmagold;
 import javax.swing.JOptionPane;
 public class MedicamentosFunciones {
-   private Medicamentos medicamentos[]=new Medicamentos[5];
-   private int opcion;
-   
-   
-   public void mostrarMenu(){
-        MedicamentosFunciones mf=new MedicamentosFunciones(); 
-      while(opcion!=3){
-         opcion=Integer.parseInt(JOptionPane.showInputDialog(null, """
-                                                                   ***CATALOGO DE MEDICAMENTOS***
-                                                                   
-                                                                   1. Llenar Informacion
-                                                                   2. Mostrar Informacion
-                                                                   3. Salir
-                                                                   
-                                                                   Digite su opcion: """));
-                         
-         switch(opcion){
-             case 1 -> {
-                mf.predefinirMedicamentos();
-                mf.llenarArreglo();
-             }
-             case 2 -> {
-                mf.mostrarArreglo();
-             }
-             case 3 -> {
-                System.exit(0);
-             }
-             default -> {
-                JOptionPane.showMessageDialog(null,
-                        "OPCION INCORRECTA!");
-             }
-         }
-      }
-   }
-      
-      
-public void predefinirMedicamentos(){
-        medicamentos[0]=new Medicamentos("PepoBismol","Analgesico","12/12/12",100);
-        medicamentos[1]=new Medicamentos("Isoface","Otro","12/12/23",160);
-        
-    }
+    // constructor
     
-    public void llenarArreglo(){
-        int x;
-        for(x=2;x<5;x++){
-            
-            Medicamentos mf=new Medicamentos();
-            
-            mf.setNombre(JOptionPane.showInputDialog(null,"Digite el nombre del medicamento: "));
-            mf.setCategoria(JOptionPane.showInputDialog(null, "Digite la categoria del medicamento;\n-Analgesico\n-Antibiotico\nOtro\n->"));
-            mf.setFecha(JOptionPane.showInputDialog(null, "Digite la fecha de caducacion del medicamento: "));
-            mf.setPrecio(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite el precio del medicamento: ")));
-            medicamentos[x]=mf;
-            
-        }
+    public MedicamentosFunciones(Medicamentos listaMedicamentos[]) {
+        mostrarMenu(listaMedicamentos);
     }
-    public void mostrarArreglo(){
-        int x;
-        String s="";
-        for(x=0;x<5;x++){
-            s=s+medicamentos[x].getNombre()+" "+medicamentos[x].getCategoria()+" "+medicamentos[x].getFecha()+medicamentos[x].getPrecio()+"\n";
-            
+
+    
+    public void mostrarMenu(Medicamentos listaMedicamentos[]){
+        boolean running = true;
+          while(running){
+             int opcion=Integer.parseInt(JOptionPane.showInputDialog(null, """
+                                                                       **CATALOGO DE MEDICAMENTOS**
+
+                                                                       1. Llenar Informacion
+                                                                       2. Mostrar Informacion
+                                                                       3. Salir
+
+                                                                       Digite su opcion: """));
+
+             switch(opcion){
+                 case 1 -> {
+                    agregarMedicamento(listaMedicamentos);
+                 }
+                 case 2 -> {
+                    mostrarMedicamentos(listaMedicamentos);
+                 }
+                 case 3 -> {
+                    running = false;
+                 }
+                 default -> {
+                    JOptionPane.showMessageDialog(null,
+                            "OPCION INCORRECTA!");
+                 }
+             }
+          }
+       }
+
+    public void agregarMedicamento(Medicamentos listaMedicamentos[]) {
+        int indice = listaMedicamentos.length - 1;
+        int indice_agregar = 0;
+        for(int x=indice;x>=0;x--){
+            if (listaMedicamentos[x].getNombre() == "") {
+                indice--;
+            } else {
+                indice_agregar = indice + 1;
+                listaMedicamentos[indice_agregar].setNombre(JOptionPane.showInputDialog(null,"Digite el nombre del medicamento: "));
+                listaMedicamentos[indice_agregar].setCategoria(JOptionPane.showInputDialog(null, "Digite la categoria del medicamento"));
+                listaMedicamentos[indice_agregar].setPrecio(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite el precio del medicamento: ")));
+                break;
+            }
         }
-        JOptionPane.showMessageDialog(null,"Los datos de las casas fabricantes son: \n"+s);
+        JOptionPane.showMessageDialog(null, "Se agrego el medicamento: " + listaMedicamentos[indice_agregar].getNombre());
+    }
+
+
+    public void mostrarMedicamentos(Medicamentos listaMedicamentos[]){
+        for(int x=0;x<listaMedicamentos.length;x++){
+            String s="";
+            if (listaMedicamentos[x].getNombre() != "") {
+                s += "Nombre medicamento " + listaMedicamentos[x].getNombre() + "\nCategoria: " + listaMedicamentos[x].getCategoria() + "\nPrecio: " + listaMedicamentos[x].getPrecio()
+                        + "\nCantidad disponible: " + listaMedicamentos[x].getCantidad();
+                JOptionPane.showMessageDialog(null,s);
+            } else {
+                break;
+            }
+        }
     }
 }
-
