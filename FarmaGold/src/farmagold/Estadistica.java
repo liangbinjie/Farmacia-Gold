8aca481fc29cd499ff8a6d9133a00e87613ddb18
Aaron Bustos <aaronj.bustos@gmail.com>
2022-08-24 17:38:35 -0600

    crea la clase estadistica

---
+++ FarmaGold/src/farmagold/Estadistica.java
@@ -0,0 +1,11 @@
+package farmagold;
+import javax.swing.*;
+public class Estadistica {
+    private double estadisticas[][]=new double [1][7];
+    
+//Dentro del for, agregar montos de cada factura, luego otro for loop para sumar todo == venta diaria   
+    public void CalculoDiario(){
+        for(){
+            
+    }
+}
