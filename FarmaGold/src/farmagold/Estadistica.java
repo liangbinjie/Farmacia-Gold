926f47b4dea66154c9e5c43029c01898e8493d73
Aaron Bustos <aaronj.bustos@gmail.com>
2022-08-24 23:43:04 -0600

    update estadistica

--- FarmaGold/src/farmagold/Estadistica.java
+++ FarmaGold/src/farmagold/Estadistica.java
@@ -3,11 +3,14 @@ import javax.swing.*;
 public class Estadistica {
     private double estadisticas[][]=new double [1][7];
     
-//Dentro del for, agregar montos de cada factura, luego otro for loop para sumar todo == venta diaria   
+//Dentro del for, agregar montos de cada factura, luego otro for loop para sumar todo. eso == venta diaria   
     public void CalculoDiario(){
         for(int x=0;x<.getMonto();x++){
-            
+        
+       }
     }
-  }
-
+//Sumar cada dia, ej 7 columnas [7], crear variable promedio/7 = promedio de una semana.
+    public void PromedioDiario(){
+        
+    }
 }
