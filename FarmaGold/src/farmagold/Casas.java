09ae4f7a9a3da4414a15a4ba049025a075416e9d
liangbinjie <105888522+AJBustos28@users.noreply.github.com>
2022-08-25 11:20:50 -0600

    Update Casas.java

--- FarmaGold/src/farmagold/Casas.java
+++ FarmaGold/src/farmagold/Casas.java
@@ -1,22 +1,23 @@
 package farmagold;
 public class Casas {
-
-    public Casas(String nombre, String paisOrigen, long edadAntiguedad) {
-        this.nombre = nombre;
-        this.paisOrigen = paisOrigen;
-        this.edadAntiguedad = edadAntiguedad;
-    }
-    
     private String nombre;
     private String paisOrigen;
-    private long edadAntiguedad;
+
+    
+    // constructor
+    public Casas(String nombre, String paisOrigen) {
+        this.nombre = nombre;
+        this.paisOrigen = paisOrigen;
+    }
     
     public Casas(){
         this.nombre="";
         this.paisOrigen="";
-        this.edadAntiguedad=0;
         
     }
+    
+    
+    // Getters and setters
 
     public String getNombre() {
         return nombre;
@@ -32,13 +33,5 @@ public class Casas {
 
     public void setPaisOrigen(String paisOrigen) {
         this.paisOrigen = paisOrigen;
-    }
-
-    public long getEdadAntiguedad() {
-        return edadAntiguedad;
-    }
-
-    public void setEdadAntiguedad(long edadAntiguedad) {
-        this.edadAntiguedad = edadAntiguedad;
     }
 }
